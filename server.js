ff1118ee367660b5b4c54150955b1c3c6f918f3f
Linda <lexicon736@gmail.com>
2022-10-06 23:50:26 -0400

    Additions Made

--- server.js
+++ server.js
@@ -14,10 +14,17 @@ app.use(favicon(path.join(__dirname, 'build', 'favicon.ico')));
 app.use(express.static(path.join(__dirname, 'build')));
 
 
-// Put API routes here, before the "catch all" route
+//Create Read Update Destroy routes go here (Index, Show, Create, Update, Delete) -- (New and Edit go on the front end (And kind of Tndex & Show))
+
+//Index and Show on the express server grab the data from the database that's needed and send it to the front end to be rendered there.
+
+
+//Put API routes here, before the "catch all" route
 
 // The following "catch all" route (note the *) is necessary
-// to return the index.html on all non-AJAX requests
+// to return the index.html on all non-AJAX requests. If the express server doesn't match anything, it sends it over to react.
+
+
 app.get('/*', (req, res) => {
     res.sendFile(path.join(__dirname, 'build', 'index.html'));
   });
